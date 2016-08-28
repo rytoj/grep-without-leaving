e642085ff8dca86f2b355a79a85798b0d4d6e39a
rytoj <rytoj@users.noreply.github.com>
2016-08-28 15:22:16 +0300

    Create gwl.sh

---
+++ gwl.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+DATA="sample.txt"
+
+#
+function recognize_and_run(){
+	if [[ "$type" == "file" ]]; then
+		grep -i --color -m 25  "$word" "$DATA"
+	elif [[ "$type" == "program" ]]; then
+		"$DATA" | grep -i --color -m 25  "$word" 
+	else
+		grep -i --color -m 25  "$word" "$DATA"
+	fi
+	
+}
+
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  echo "Usage: `basename $0` -f [file-name]"
+  exit 1
+fi
+
+if [[ "$1" == "-f"  && -e "$2" ]]; then
+	DATA="$2"
+	type="file"
+	
+fi
+
+if [ $(which "$2") ]; then
+	DATA="$2"
+	type="program"
+fi
+	
+
+
+
+clear
+char=" "
+echo -n "Search_: "
+while IFS= read -rn1 char;  do
+	clear
+	word="$word$char"
+
+#Check if Backspace is pressed
+if [[ $char == $'\177' ]]
+    then
+        word=${word%?}
+        word=${word%?}
+    fi
+#Check for empty string
+	if [[ -z "$word" ]]; then
+		echo "--"
+	else
+		recognize_and_run
+	fi
+	echo -n "Search: "
+	echo -n "$word"
+
+done
